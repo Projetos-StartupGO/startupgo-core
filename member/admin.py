b73fdcebdc8ad786574f7ebe2daa6f217a71f631
Hyper Agencia <webmaster@hyperagencia.com.br>
2020-01-18 05:30:36 -0300

    ignore sqlite, models

--- member/admin.py
+++ member/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from member import models
+
+admin.site.register(models.Member)
+admin.site.register(models.Community)
+admin.site.register(models.ServiceOffer)
+admin.site.register(models.Company)
+admin.site.register(models.Event)
--- member/models.py
+++ member/models.py
@@ -1,10 +1,11 @@
+from uuid import uuid4
 from django.db import models as models
 from django.utils.translation import gettext as _
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 
 
 class BaseModel(models.Model):
-    id = models.UUIDField(verbose_name=_('uuid'), editable=False, unique=True, primary_key=True)
+    id = models.UUIDField(verbose_name=_('id'), default=uuid4, editable=False, unique=True, primary_key=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -13,43 +14,100 @@ class BaseModel(models.Model):
         ordering = ['-created']
 
 
+class MemberManager(UserManager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
+class Skill(BaseModel):
+    name = models.SlugField()
+
+    class Meta(BaseModel.Meta):
+        verbose_name = _('Habilidade')
+        verbose_name_plural = _('Habilidades')
+
+    def __str__(self):
+        return self.name
+
+
 class Member(BaseModel, AbstractUser):
+    skill = models.ManyToManyField(Skill, related_name='member_skills')
+
+    objects = MemberManager()
+    original_manager = UserManager()
 
     def delete(self, using=None, keep_parents=False):
         raise Exception(_('Membros não podem ser apagados'))
 
-    class Meta:
+    class Meta(BaseModel.Meta):
         verbose_name = _('Membro')
         verbose_name_plural = _('Membros')
+
+    def __str__(self):
+        return self.email
 
 
 class Community(BaseModel):
     name = models.SlugField()
     description = models.CharField(max_length=255)
-    owner = models.ForeignKey(Member, on_delete=models.PROTECT)
-    members = models.ForeignKey(Member, on_delete=models.PROTECT)
+    owner = models.ForeignKey(Member, on_delete=models.PROTECT, related_name='community_owners')
+    member = models.ManyToManyField(Member, related_name='community_members')
 
-    class Meta:
+    class Meta(BaseModel.Meta):
         verbose_name = _('Comunidade')
         verbose_name_plural = _('Comunidades')
 
+    def __str__(self):
+        return self.name
 
-class Company(models.Model):
-    name = models.SlugField(populate_from='name', blank=True)
-    owner = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="companys")
-    members = models.ManyToManyField(Member, on_delete=models.PROTECT)
 
-    class Meta:
+class Company(BaseModel):
+    name = models.SlugField()
+    owner = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="company_owners")
+    member = models.ManyToManyField(Member, related_name='company_members')
+
+    class Meta(BaseModel.Meta):
         verbose_name = _('Empresa')
         verbose_name_plural = _('Empresas')
 
+    def __str__(self):
+        return self.name
 
-class Event(models.Model):
+
+class Event(BaseModel):
     name = models.SlugField()
     address = models.CharField(max_length=255)
-    owner = models.ForeignKey(Member, on_delete=models.PROTECT)
-    communities = models.ForeignKey(Company, on_delete=models.PROTECT)
+    description = models.CharField(max_length=255)
+    owner = models.ForeignKey(Member, on_delete=models.PROTECT, related_name='event_owners')
+    community = models.ForeignKey(Community, on_delete=models.PROTECT, related_name='event_communities')
 
-    class Meta:
+    class Meta(BaseModel.Meta):
         verbose_name = _('Evento')
         verbose_name_plural = _('Eventos')
+
+    def __str__(self):
+        return self.name
+
+
+class Interest(BaseModel):
+    name = models.SlugField()
+
+    class Meta(BaseModel.Meta):
+        verbose_name = _('Interesse')
+        verbose_name_plural = _('Interesses')
+
+    def __str__(self):
+        return self.name
+
+
+class ServiceOffer(BaseModel):
+    name = models.SlugField()
+    description = models.CharField(max_length=255)
+    skill = models.ManyToManyField(Skill, related_name='service_offer_skills')
+
+    class Meta(BaseModel.Meta):
+        verbose_name = _('Oferta de serviço')
+        verbose_name_plural = _('Oferta de serviços')
+
+    def __str__(self):
+        return self.name
